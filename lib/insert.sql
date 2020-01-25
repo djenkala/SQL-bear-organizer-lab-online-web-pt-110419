9a78865f67c9e97dc3769a7dba098b85f7a68c85
Daniel Jenkala <djenkala@yahoo.com>
2020-01-25 10:42:55 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,10 +6,10 @@ INSERT INTO bears VALUES (3, "Tabitha", 10, "Female", "Brown", "Sleepy", 0);
 
 INSERT INTO bears VALUES (4, "Sergeant Brown", 7, "Male", "Brown", "Strict", 0);
 
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 2, "Female", "White", "Agressive", 1);
+INSERT INTO bears VALUES (5, "Melissa", 2, "Female", "White", "Agressive", 1);
 
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 15, "Male", "Black", "Mean", 1);
+INSERT INTO bears VALUES (6, "Grinch", 15, "Male", "Black", "Mean", 1);
 
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 10, "Female", "Brown", "Sweet", 0);
+INSERT INTO bears VALUES (7, "Wendy", 10, "Female", "Brown", "Sweet", 0);
 
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 8, "Male", "Brown", "Violent", 1);
+INSERT INTO bears VALUES (8, NULL, 8, "Male", "Brown", "Violent", 1);
