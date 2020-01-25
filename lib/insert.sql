7fa3123d162a9754d588b50795a565a16fc6ef43
Daniel Jenkala <djenkala@yahoo.com>
2020-01-25 10:42:37 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,10 +1,10 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 5, "Male", "Brown", "Sweet", 1);
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 5, "Male", "Brown", "Sweet", 1);
 
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 2, "Male", "Black", "Rowdy", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 2, "Male", "Black", "Rowdy", 1);
 
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 10, "Female", "Brown", "Sleepy", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 10, "Female", "Brown", "Sleepy", 0);
 
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 7, "Male", "Brown", "Strict", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 7, "Male", "Brown", "Strict", 0);
 
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 2, "Female", "White", "Agressive", 1);
 
